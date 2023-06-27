6cd5a47e1384719d5eeeb0e369b8cd1c21bda634
Денис Тавабилов <dagger1233@gmail.com>
2023-06-27 07:54:34 +0500

    переключение стилей

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-Vue.createApp({
+let vm = Vue.createApp({
   data() {
     return {
-        
-    }
+      isPurple: false,
+      selected: "",
+    };
+  },
+  computed: {
+    setClass() {
+      return {
+        purple: this.isPurple,
+      };
+    },
   },
 }).mount("#app");
