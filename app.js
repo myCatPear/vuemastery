6d7775c93142bc05d04f24df32bacd3921ba3407
Денис Тавабилов <dagger1233@gmail.com>
2023-06-29 08:23:37 +0500

    создание компонент с помощью vm component

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const vm = Vue.createApp({
+  // data() {
+  //   return {
+  //     message: "Hello world",
+  //   };
+  // },
+  // template: `{{message}}`,
+});
+
+vm.component('hello', {
+  template: '<h1>{{message}}</h1>',
   data() {
     return {
-      message: "Hello world",
-    };
-  },
-  template: `{{message}}`,
-});
+      message:'Hello world from components!'
+    }
+  }
+})
 
 vm.mount("#app");
