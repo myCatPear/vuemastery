6fc08f3f68ea3275ff9a02b0ab297e2577972c68
Денис Тавабилов <dagger1233@gmail.com>
2023-06-29 08:13:10 +0500

    создание вью с помощью template

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
 const vm = Vue.createApp({
   data() {
     return {
-      perspective: 100,
-      rotateX: 0,
-      rotateY: 0,
-      rotateZ: 0,
+      message: "Hello world",
     };
   },
-  computed: {
-    box() {
-      return {
-        transform: `
-        perspective(${this.perspective}px)
-        rotateX(${this.rotateX}deg)
-            rotateY(${this.rotateY}deg)
-                rotateZ(${this.rotateZ}deg)`,
-      };
-    },
-  },
-  methods: {
-    reset() {
-      this.perspective = 100;
-      this.rotateY = 0;
-      this.rotateZ = 0;
-      this.rotateX = 0;
-    },
-    async copy() {
-        const text = `transofrm:${this.box.transform}`
-        await navigator.clipboard.writeText(text)
-    }
-  },
-}).mount("#app");
+  template: `{{message}}`,
+});
+
+vm.mount("#app");
