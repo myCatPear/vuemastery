a2e8965432efda51a8954fc6f47517d97f1e635c
Denis Tavabilov <dagger1233@gmail.com>
2023-07-06 14:57:07 +0500

    динамические компоненты

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
   <user :ageChangeFn="updateAgeCB" :age="age"></user>
   <app-form>
     <div>{{ age }}</div>
-    <temaplte v-slot:help>
+    <template v-slot:help>
       This is some help text
-    </temaplte>
+    </template>
     <template v-slot:fields>
       <input type="text" placeholder="email">
       <input type="text" placeholder="username">
@@ -16,7 +16,7 @@
     </template>
   </app-form>
   <app-form>
-     <template v-slot:help>
+    <template v-slot:help>
       Contact help text
     </template>
     <template v-slot:fields>
@@ -27,12 +27,23 @@
       <button type="submit">Submit</button>
     </template>
   </app-form>
+  <hr />
+  <select v-model="componentsName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+
+  <keep-alive>
+    <component :is="componentsName" />
+  </keep-alive>
 </template>
 
 <script>
 import Greeting from "@/components/Greeting.vue";
 import User from "@/components/User.vue";
 import AppForm from "./components/AppForm.vue";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
 
 export default {
   name: "App",
@@ -40,10 +51,13 @@ export default {
     Greeting,
     User,
     AppForm,
+    About,
+    Home,
   },
   data() {
     return {
       age: 20,
+      componentsName: 'Home'
     };
   },
   methods: {
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        HomePage
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('mounted')
+    },
+    unmounted() {
+        console.log('unmounted')
+    },
+    activated() {
+        console.log('activated')
+
+    },
+    deactivated() {
+        console.log('deactivated')
+
+    }
+}
+</script>
+
+<style scoped></style>
