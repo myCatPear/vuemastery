e426fd01d5eb46b97768f3f579ddae5a32ee5002
Denis Tavabilov <dagger1233@gmail.com>
2023-07-08 00:54:35 +0500

    анимация списков

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <greeting></greeting>
+  <!-- <greeting></greeting>
   <user :ageChangeFn="updateAgeCB" :age="age"></user>
   <app-form>
     <div>{{ age }}</div>
@@ -31,13 +31,13 @@
   <select v-model="componentsName">
     <option value="Home">Home</option>
     <option value="About">About</option>
-  </select>
+  </select> -->
 
-  <keep-alive>
+  <!-- <keep-alive>
     <component :is="componentsName" />
   </keep-alive>
   <hr>
-  <button type="button" @click="flag = !flag">Toggle</button>
+  <button type="button" @click="flag = !flag">Toggle</button> -->
 
   <!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag">Hellow deniska</h2>
@@ -48,10 +48,23 @@
     <h2 v-if="flag">Hello DEN!</h2>
   </transition> -->
 
-  <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave"
+  <!-- <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @before-leave="beforeLeave"
     @leave="leave" @after-leave="afterLeave" :css="true" name="fade">
     <h2 v-if="flag">Hey den</h2>
-  </transition>
+  </transition> -->
+
+  <button @click="addItem">Add</button>
+
+  <ul>
+    <transition-group name="fade">
+
+    <li v-for="(number,index) in numbers" :key="number" @click="removeItem(index)">
+      {{ number }}
+
+    </li>
+    </transition-group>
+
+  </ul>
 </template>
 
 <script>
@@ -75,9 +88,18 @@ export default {
       age: 20,
       componentsName: 'Home',
       flag: true,
+      numbers:[1,2,3,4,5]
     };
   },
   methods: {
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1)
+      const index = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(index, 0, num);
+    },
+    removeItem(index) {
+      this.numbers.splice(index, 1)
+    },
     updateAgeCB(num) {
       this.age += num;
     },
@@ -123,6 +145,11 @@ export default {
 </script>
 
 <style scoped>
+li {
+  font-size: 22px;
+  cursor: pointer;
+}
+
 h2 {
   width: 400px;
   padding: 20px;
