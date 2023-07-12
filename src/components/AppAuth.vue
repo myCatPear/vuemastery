42241ec4ece8d37ef58a53f8756f7e327c7eab97
Denis Tavabilov <dagger1233@gmail.com>
2023-07-12 23:23:48 +0500

    валидация регистрации

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -81,7 +81,19 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <div
+            class="text-white text-center font-bold p-4 rounded mb-4"
+            v-if="reg_show_alert"
+            :class="reg_alert_variant"
+          >
+            {{ reg_alert_msg }}
+          </div>
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+            :initial-values="userData"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -113,45 +125,61 @@
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               />
               <error-message class="text-red-600" name="age" />
-
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  type="password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">{{ error }}</div>
+              </vee-field>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input
+              <vee-field
+                name="confirm_password"
                 type="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password"
               />
+              <error-message class="text-red-600" name="confirm_password" />
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
+              <vee-field
+                as="select"
+                name="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               >
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+                <option value="Antarctica">Antarctica</option>
+              </vee-field>
+              <error-message class="text-red-600" name="country" />
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
+              <vee-field
+                type="checkbox"
+                name="tos"
+                value="1"
+                class="w-4 h-4 float-left -ml-6 mt-1 rounded"
+              />
               <label class="inline-block">Accept terms of service</label>
+              <error-message class="text-red-600" name="tos" />
             </div>
             <button
               type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              :disabled="reg_in_submission"
             >
               Submit
             </button>
@@ -174,11 +202,18 @@
           name: 'required|min:3|max:100|alpha_spaces',
           email: 'required|min:3|max:100|email',
           age: 'required|min_value:18|max_value:100',
-          password: '',
-          confirm_password: '',
-          country: '',
-          tos: ''
-        }
+          password: 'required|min:9|max:100|excluded:password',
+          confirm_password: 'password_mismatch:@password',
+          country: 'required|country_excluded:Antarctica',
+          tos: 'tos'
+        },
+        userData: {
+          country: 'USA'
+        },
+        reg_in_submission: false,
+        reg_show_alert: false,
+        reg_alert_variant: 'bg-blue-500',
+        reg_alert_msg: 'Please wait! Your account is being created'
       }
     },
     computed: {
@@ -186,6 +221,19 @@
       ...mapWritableState(useModalStore, {
         modalVisibility: 'isOpen'
       })
+    },
+    methods: {
+      register(values) {
+        this.reg_show_alert = true
+        this.reg_in_submission = true
+        this.reg_alert_variant = 'bg-blue-500'
+        this.reg_alert_msg = 'Please wait! Your account is being created'
+
+        this.reg_alert_variant = 'bg-green-500';
+        this.reg_alert_msg = 'Success! Your account has been created!'
+
+        console.log(values)
+      }
     }
   }
 </script>
