22965fd626f198a57bf473116dabef25f6052af6
Денис Тавабилов <dagger1233@gmail.com>
2023-07-18 08:30:18 +0500

    роуты все уроки

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
 import About from '@/views/AboutView.vue'
+import Manage from '@/views/ManageView.vue'
+import useUserStore from '@/stores/user'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home
   },
   {
+    name: 'about',
     path: '/about',
     component: About
+  },
+  {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard')
+      next()
+    },
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+})
+
+router.beforeEach((to, from, next) => {
+  // console.log(to.meta)
+
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
+
+  const store = useUserStore()
+  if (store.userLoggedIn) {
+    next()
+  } else {
+    next({ name: 'home' })
+  }
 })
 
 export default router
