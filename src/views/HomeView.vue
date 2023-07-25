aaf581b66cb6b0831e68d4a2563b6bbea6917246
Денис Тавабилов <dagger1233@gmail.com>
2023-07-25 08:35:06 +0500

    passing values to directives

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,7 +26,7 @@
     <!-- Main Content -->
     <section class="container mx-auto">
       <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon="'headphones-alt'">
+        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon:full="'headphones-alt'">
           <span class="card-title">Songs</span>
           <!-- Icon -->
           <!-- <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i> -->
