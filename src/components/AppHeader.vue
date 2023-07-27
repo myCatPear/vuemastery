6d70c2549633caf79876c082e86ba8c2f4f0507f
Денис Тавабилов <dagger1233@gmail.com>
2023-07-27 08:18:29 +0500

    pwa

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -30,6 +30,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">
+              {{ currentLocale }}
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -43,8 +50,11 @@
   export default {
     name: 'AppHeader',
     computed: {
-      ...mapStores(useModalStore, useUserStore)
-      // ...mapWritableState(useModalStore, ['isOpen'])
+      ...mapStores(useModalStore, useUserStore),
+      // ...mapWritableState(useModalStore, ['isOpen']),
+      currentLocale() {
+        return this.$i18n.locale === 'ru' ? 'Russian' : 'English'
+      }
     },
     methods: {
       toggleAuthModal() {
@@ -57,6 +67,9 @@
         if (this.$route.meta.requiresAuth) {
           this.$router.push({ name: 'home' })
         }
+      },
+      changeLocale() {
+        this.$i18n.locale = this.$i18n.locale === 'ru' ? 'en' : 'ru'
       }
     }
   }
